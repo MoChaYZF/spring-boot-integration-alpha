1f6c00138d3c845d1dcf2476dfff5fb545c78f5c
MoChaYZF <1358192165@qq.com>
2020-03-02 21:50:07 +0800

    初次提交

---
+++ src/main/java/top/yangzefeng/integration/IntegrationAlphaApplication.java
@@ -0,0 +1,17 @@
+package top.yangzefeng.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ *
+ * @author MoCha
+ * @date 202003/02
+ */
+@SpringBootApplication
+public class IntegrationAlphaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationAlphaApplication.class, args);
+    }
+}
---
+++ src/test/java/top/yangzefeng/integration/IntegrationAlphaApplicationTests.java
@@ -0,0 +1,14 @@
+package top.yangzefeng.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class IntegrationAlphaApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
