f25c0e2d18105ac6bdc4a28dacfb5e8a2488676a
MoChaYZF <1358192165@qq.com>
2020-03-28 21:45:46 +0800

    解决请求url携带了特殊符号的参数，导致出现状态码400的错误

--- src/main/java/top/yangzefeng/integration/common/config/WebServerCustomizationBean.java
+++ src/main/java/top/yangzefeng/integration/common/config/WebServerCustomizationBean.java
@@ -1,29 +1,22 @@
 package top.yangzefeng.integration.common.config;
 
-import io.undertow.server.DefaultByteBufferPool;
-import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
 
 /**
- * 解决Buffer pool was not set on WebSocketDeploymentInfo问题警告
- * 看提示是让设置一下buffer pool，不然就使用默认的，这个警告不影响使用，但是看着别扭
- * 官方文档：
- * <a https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-programmatic-embedded-container-customization></a>
+ * 主要解决请求url携带了特殊符号的参数，导致出现状态码400的错误
+ * 说明：
+ * 1. Tomcat的新版本中增加了一个新特性，就是严格按照 RFC 3986规范进行访问解析，而 RFC 3986规范定义了Url中只允许包含英文字母（a-zA-Z）、数字（0-9）、-_.~4个特殊字符以及所有保留字符(RFC3986中指定了以下字符为保留字符：! * ’ ( ) ; : @ & = + $ , / ? # [ ])。
  *
  * @author MoCha
  * @date 2019/9/30
  */
 @Component
-public class WebServerCustomizationBean implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+public class WebServerCustomizationBean implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
     @Override
-    public void customize(UndertowServletWebServerFactory factory) {
-        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
-            WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
-            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 1024));
-            deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
-        });
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setAttribute("relaxedQueryChars", "[]"));
     }
 }
 
